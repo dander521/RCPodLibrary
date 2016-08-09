3881395fdb6d1c3cf554f97fb6ec95d67640c33a
RogerChen <123020990@qq.com>
2016-08-09 15:11:24 +0800

    modify

--- Example/RCPodLibrary/RCViewController.m
+++ Example/RCPodLibrary/RCViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "RCViewController.h"
+#import <RCPodLibrary/PrintLog.h>
 
 @interface RCViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewOne;
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewTwo;
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewThree;
 
 @end
 
@@ -17,7 +21,18 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    [PrintLog printLog];
+    
+    NSString *strResourcesBundle = [[NSBundle mainBundle] pathForResource:@"RCPodLibrary.bundle" ofType:nil];
+    // 找到对应images夹下的图片
+    NSString *strImage1 = [[NSBundle bundleWithPath:strResourcesBundle] pathForResource:@"1.jpg" ofType:nil];
+    NSString *strImage2 = [[NSBundle bundleWithPath:strResourcesBundle] pathForResource:@"2.jpg" ofType:nil];
+    NSString *strImage3 = [[NSBundle bundleWithPath:strResourcesBundle] pathForResource:@"3.jpg" ofType:nil];
+    
+    self.imageViewOne.image = [UIImage imageWithContentsOfFile:strImage1];
+    self.imageViewTwo.image = [UIImage imageWithContentsOfFile:strImage2];
+    self.imageViewThree.image = [UIImage imageWithContentsOfFile:strImage3];
 }
 
 - (void)didReceiveMemoryWarning
